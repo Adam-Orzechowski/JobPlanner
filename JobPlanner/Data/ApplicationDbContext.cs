f6a1e573ac3c9492ed9620e820cf9e1ebdafc2ef
Adam Orzechowski <adam@orzechowscy.eu>
2025-01-29 23:28:28 +0100

    Add project files.

---
+++ JobPlanner/Data/ApplicationDbContext.cs
@@ -0,0 +1,57 @@
+﻿using JobPlanner.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobPlanner.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = "5e174d1e-2b9f-48fe-9561-1149894920f5",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Id = "22d1a66c-ccd8-4c1e-8763-a896c287ea2c",
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+            );
+            builder.Entity<ApplicationUser>().HasData(
+                new ApplicationUser
+                {
+                    Id = "22d1a66c-ccd8-4c1e-8763-a896c287ea2c",
+                    UserName = "admin@localhost",
+                    NormalizedUserName = "ADMIN@LOCALHOST",
+                    Email = "admin@localhost",
+                    NormalizedEmail = "ADMIN@LOCALHOST",
+                    PasswordHash = new PasswordHasher<ApplicationUser>().HashPassword(null, "admin"),
+                    Image = File.ReadAllBytes("wwwroot\\assets\\images\\users\\avatar-1.jpg"),
+                    EmailConfirmed = true
+                }
+            );
+            builder.Entity<IdentityUserRole<string>>().HasData(
+
+                new IdentityUserRole<string>
+                {
+                    RoleId = "5e174d1e-2b9f-48fe-9561-1149894920f5",
+                    UserId = "22d1a66c-ccd8-4c1e-8763-a896c287ea2c"
+                }
+            );
+        }
+    }
+
+}
---
+++ JobPlanner/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JobPlanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
